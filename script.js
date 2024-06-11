c4d526b226e9c2e1ac986e1c3093812c08560a54
pedrogava <rm551043@fiap.com.br>
2024-06-11 10:35:56 -0300

    Exercicio 1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Exercicio 1
+let variavelUndefined;
+const variavelVazia = ''
+const variavelValor = 'Hello World'
+
+console.log(variavelUndefined)
+console.log(variavelVazia)
+console.log(variavelValor)
+
