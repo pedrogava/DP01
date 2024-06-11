b6459fc8023a1bab89b4dfe4da36209291238d21
pedrogava <rm551043@fiap.com.br>
2024-06-11 10:41:31 -0300

    exercicio2

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ console.log(variavelUndefined)
 console.log(variavelVazia)
 console.log(variavelValor)
 
+// Exercicio 2
+let a = 5 
+let b = 25
+
+console.log(`a != b: ${a != b}`)
+console.log(`a == b: ${a == b}`)
+console.log(`b >= a: ${b >= a}`)
+console.log(`b<=a: ${b <= a}`)
+
