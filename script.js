4186c50cf9bcacd31c7eafee4d89c94f8424b2c0
pedrogava <rm551043@fiap.com.br>
2024-06-11 10:45:30 -0300

    Exercicio 3

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ console.log(`a == b: ${a == b}`)
 console.log(`b >= a: ${b >= a}`)
 console.log(`b<=a: ${b <= a}`)
 
+//Exercicio 3
+
+function calcularIMC(peso, altura) {
+    let imc = peso / (altura * altura);
+    let mensagem;
+    if (imc < 18.5) {
+        mensagem = "Abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        mensagem = "Peso ideal";
+    } else {
+        mensagem = "Acima do peso";
+    }
+    return `IMC: ${imc.toFixed(2)} - ${mensagem}`;
+}
+output.innerHTML += `<p>${calcularIMC(70, 1.75)}</p>`;
